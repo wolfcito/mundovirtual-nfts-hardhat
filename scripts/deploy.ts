9bbbd173ff2ba6c5dcf9721c7258bcc6c952cf4e
Luis Fernando Ushiña <guffenix@gmail.com>
2022-08-17 22:03:12 -0500

    deploy of MundoVirtualPunk

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,22 @@
 import { ethers } from 'hardhat'
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000)
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS
+const deploy = async () => {
+  const [deployer] = await ethers.getSigners()
+  const MundoVirtualPunk = await ethers.getContractFactory('MundoVirtualPunk')
 
-  const lockedAmount = ethers.utils.parseEther('0.000000001')
+  const _maxSuply = 100
 
-  const Lock = await ethers.getContractFactory('Lock')
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount })
+  console.log('Deploy contract with the account: ', deployer.address)
 
-  await lock.deployed()
+  const deployed = await MundoVirtualPunk.deploy(_maxSuply)
 
-  console.log('Lock with 0.000000001 ETH deployed to:', lock.address)
+  // Start deployment, returning a promise that resolves to a contract object
+  console.log('Contract deployed to address:', deployed.address)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error)
-  process.exitCode = 1
-})
+deploy()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
